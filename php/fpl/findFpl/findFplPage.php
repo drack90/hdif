42e8f753c3ef023ac2bd120acee95769185ff69d
drack90 <38080297+drack90@users.noreply.github.com>
2019-10-02 22:04:59 +0500

    Исправлены имена передаваемые в обработчик поиска

--- php/fpl/findFpl/findFplPage.php
+++ php/fpl/findFpl/findFplPage.php
@@ -1,5 +1,6 @@
 <?php require $_SERVER["DOCUMENT_ROOT"] . "/php/config/config.php"; ?>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/php/require/requireplugins.php';?>
+
 <div id="findFplPages">
 <div class="row">
     <div class="col-1"></div>
@@ -10,34 +11,49 @@
             <div class="form-group">
                 <form method="POST" id="findFpl">
                     <div>
-                        <label for="fplname">Имя FPL</label>
-                        <input class="form-control" id="fplname" name="fplname" rows="1"></input>
+                        <label for="fplName">Имя FPL</label>
+                        <input class="form-control" id="fplnName" name="fplName" rows="1"></input>
                     </div>
 
                     <div class="form-group">
-                        <label for="departure">Взлет</label>
-                        <input class="form-control" id="departure" name="departure" rows="1"></input>
+                        <label for="field13">Взлет</label>
+                        <input class="form-control" id="field13" name="field13" rows="1"></input>
                     </div>
 
                     <div class="form-group">
-                        <label for="arrival">Посадка</label>
-                        <input class="form-control" id="arrival" rows="1" name="arrival"></input>
+                        <label for="field16">Посадка</label>
+                        <input class="form-control" id="field16" rows="1" name="field16"></input>
                     </div>
 
                     <div class="form-group">
-                        <label for="route">Маршурт</label>
-                        <input class="form-control" id="route" rows="1" name="route"></input>
+                        <label for="field15">Маршурт</label>
+                        <input class="form-control" id="field15" rows="1" name="field15"></input>
                     </div>
                     <div class="form-group">
-                        <label for="remark">Поиск по 18 полю</label>
-                        <input class="form-control" id="remark" rows="1" name="remark"></textarea>
+                        <label for="field18">Поиск по 18 полю</label>
+                        <input class="form-control" id="field18" rows="1" name="field18"></textarea>
                     </div>
                     <div class="form-group">
-                        <label for="comment">Поиск по комментарию</label>
+                        <label for="commentaries">Поиск по комментарию</label>
                         <input class="form-control" id="commentaries" rows="1" name="commentaries"></textarea>
                     </div>
 
-                    <input id="searchFpl" type="submit" class="btn btn-primary">
+
+                        <div class="row">
+                            <div class="col-6 ">
+                                        <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="author" name="author">
+                                        <label class="custom-control-label" for="author" >Только Мои</label>
+                                    </div>
+                            </div>
+
+                            <div class="col-6 flex-row ">
+                                <input id="searchFpl" type="submit" class="btn btn-primary">
+                            </div>
+                        </div>
+
+
+
                 </form>
             </div>
         </div>
@@ -47,6 +63,7 @@
         <div class="results" id="results"></div></div>
 </div>
 </div>
+
 <script type="text/javascript">
 
 
@@ -65,3 +82,4 @@
         event.preventDefault();
 
     });
+    </script>
