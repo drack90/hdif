f20c402f0ac4e7acb3afab940226d91c294e105b
drack90 <38080297+drack90@users.noreply.github.com>
2019-11-01 14:40:29 +0500

    Скрипт удаленя записи из журнала

---
+++ php/journal/deletePostInJournal.php
@@ -0,0 +1,20 @@
+<?php
+
+require $_SERVER["DOCUMENT_ROOT"] . "/php/config/config.php";
+
+
+$post_id = $_POST['buttonValue'];
+
+
+$sql = 'DELETE FROM journal WHERE id = :id';
+$params = [ ':id' =>$post_id];
+
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params);
+
+echo "Запись удалена.";
+
+
+?>
+
