88509b0168a46fa73a1e26e3cb168caadf5034e0
drack90 <38080297+drack90@users.noreply.github.com>
2019-11-16 01:05:10 +0500

    Создан AJAX обработчик событий на отправку данных в обработчик редактирования FPL

--- js/findFpl/editFplAJAX.js
+++ js/findFpl/editFplAJAX.js
@@ -1,42 +1,49 @@
-$(document).ready(function () {
+    $('button#editFplInBD').on('click', function () { //при нажатии на кнопку с классом close
 
-    $('button.editFpl').on('click', function () { //при нажатии на кнопку с классом close
-        $('#editFPLModal').modal('show');
+
+        $.ajax({
+            url: "/php/fpl/editFpl/editFplProcessor.php",
+            type: "POST",
+            data: $('#editFplForm').serialize(),
+            dataType: "html",
+            success: function (data) {
+                console.log(data);
+
+            }
+        });
+
+
+        //     var buttonValue = { //создается переменная которая хранит значение value кнопки
+        //         'buttonValue': this.value
+        //     };
+        //     var r = confirm('точно удалить?'); //функция confirm производит запрос у порльзователя о удалении данных
+        //
+        //     if(r == true) {
+        //
+        //         $.ajax({
+        //             data: buttonValue,
+        //             url: "/php/fpl/deleteFpl/deleteFplProcessor.php",
+        //             type: "POST",
+        //             dataType: "html",
+        //             success: function (html) {
+        //                 console.log(buttonValue);
+        //                 alert(html);
+        //                 //после удаления запись, производит повторную загрузку данных из БД
+        //                 $.ajax({
+        //                     url: "/php/fpl/findFpl/findFplProcessor.php",
+        //                     type: "POST",
+        //                     data: $('#findFpl').serialize(),
+        //                     dataType: "html",
+        //                     success: function (data) {
+        //                         $('#results').html(data);
+        //                     }
+        //                 });
+        //             }
+        //         });
+        //     } else{
+        //         console.log('отмена удаления');
+        //     }
+        // });
+
+
     });
-
-
-
-    //     var buttonValue = { //создается переменная которая хранит значение value кнопки
-    //         'buttonValue': this.value
-    //     };
-    //     var r = confirm('точно удалить?'); //функция confirm производит запрос у порльзователя о удалении данных
-    //
-    //     if(r == true) {
-    //
-    //         $.ajax({
-    //             data: buttonValue,
-    //             url: "/php/fpl/deleteFpl/deleteFplProcessor.php",
-    //             type: "POST",
-    //             dataType: "html",
-    //             success: function (html) {
-    //                 console.log(buttonValue);
-    //                 alert(html);
-    //                 //после удаления запись, производит повторную загрузку данных из БД
-    //                 $.ajax({
-    //                     url: "/php/fpl/findFpl/findFplProcessor.php",
-    //                     type: "POST",
-    //                     data: $('#findFpl').serialize(),
-    //                     dataType: "html",
-    //                     success: function (data) {
-    //                         $('#results').html(data);
-    //                     }
-    //                 });
-    //             }
-    //         });
-    //     } else{
-    //         console.log('отмена удаления');
-    //     }
-    // });
-
-
-})
