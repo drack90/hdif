6a46f960d64884e826323c95d604b9b86c21e1da
drack <drack702@gmail.com>
2019-11-23 18:49:55 +0500

    Добавлена форма редактирования FPL, которые полныеы, а так же те, которые сохранены как обычный текст.

--- php/fpl/editFpl/editFplForm.php
+++ php/fpl/editFpl/editFplForm.php
@@ -2,6 +2,7 @@
 
 require $_SERVER["DOCUMENT_ROOT"] . "/php/config/config.php";
 require $_SERVER['DOCUMENT_ROOT'] . '/php/class/autoloadClass.php';
+require $_SERVER["DOCUMENT_ROOT"] . "/php/require/requireplugins.php";
 
 
 
@@ -20,6 +21,92 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute($params);
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)):
 
+    if ($row['notFPL']== true){
+        ?>
+
+        <div class="modal fade bd-example-modal-lg" id="editFPLModal" tabindex="-1" role="dialog" aria-labelledby="editFPLModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+
+                    <div class="modal-header">
+                        <h3 class="modal-title h3 mb-3 font-weight-normal" id="exampleModalLongTitle" >Редактирование FPL</h3>
+                        <button type="button" class="close right" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+
+                    </div>
+
+                    <div class="modal-body">
+
+                        <form class="col 10 center" id="editFplForm">
+
+
+                            <div class="form-group">
+                                <label for="FPL_name" id="FPL_name"  class="h5">Название плана</label>
+                                <textarea class="form-control" id="FPL_name" rows="1" name="FPL_name"><?php print_r(strip_tags($row['fplName']));?></textarea>
+                            </div>
+
+
+                            <div class="form-group">
+                                <label for="field15" id="field15__label" class="h6">Сохраняемый текст </label>
+                                <textarea class="form-control" id="field15" name="field15" rows="8"><?php print_r(strip_tags($row['field15'])); ?></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="commentaries" id="commentaries" class="h6" > <b>Комментарий</b> </label>
+                                <textarea class="form-control" id="commentaries" name="commentaries" rows="1" ><?php print_r(strip_tags($row['commentaries'])); ?></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="exempleCategory">Дирекция</label>
+                                <select class="form-control form-control-sm" id="direction" name="direction">
+                                    <option>Тюмень</option>
+                                    <option>Сургут</option>
+                                    <option>Игарка</option>
+                                    <option>Хабаровск</option>
+                                    <option>Иркутск</option>
+                                    <option>Ноябрьск</option>
+                                    <option>Омск</option>
+                                    <option>Усинск</option>
+                                    <option>Уфа</option>
+                                    <option>Тобольск</option>
+
+                                </select>
+                            </div>
+
+                            <div style="display: none">
+                                <div class="form-group text-hide hide">
+                                    <label for="bdid" id="bdid" class="h6 text-hide hide" > <b>id</b> </label>
+                                    <textarea class="form-control text-hide hide" id="bdid" name="bdid" rows="1" ><?php print_r(strip_tags($row['id'])); ?></textarea>
+                                </div>
+                                <div class="form-group text-hide hide">
+                                    <label for="notStsndartFPL1" id="notStsndartFPL1" class="h6 text-hide hide" > <b>1</b> </label>
+                                    <textarea class="form-control text-hide hide" id="notStsndartFPL1" name="notStsndartFPL1" rows="1" >1</textarea>
+                                </div>
+                            </div>
+
+
+                            <hr>
+
+
+                            <div class="row justify-content-between">
+                                <div class="col-6">
+                                    <button type="submit" id="editFplInBD" class="btn btn-primary" value="<?php print_r($row['id']); ?>">Отправить</button>
+                                </div>
+                                <div class="col-6-right">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal" value="<?php print_r($row['id']); ?>">Закрыть</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="<?$_SERVER['DOCUMENT_ROOT'];?>/js/findFpl/editFplAJAX.js"></script>
+
+
+    <?php
+exit;
+}
     ?>
 
 
@@ -76,7 +163,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)):
                         </div>
                         <div class="col-auto">
                             <div class="form-group">
-                                <label for="alternative1" id="alternative1__label" class="h6" >Запасной 1 </label>
+                                <label for="alternative1" id="alternative1__label" class="h6" >Запасные </label>
                                 <textarea class="form-control" id="alternative" name="alternative" rows="1" ><?php print_r(strip_tags($row['alternative'])); ?></textarea>
                             </div>
                         </div>
@@ -89,16 +176,33 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)):
                         <textarea class="form-control" id="field18" name="field18" rows="5" ><?php print_r(strip_tags($row['field18'])); ?></textarea>
                     </div>
 
-                    <div class="custom-control custom-switch">
-                        <input type="checkbox" class="custom-control-input" id="notStsndartFPL" name="notStandartFPL">
-                        <label class="custom-control-label" for="notStsndartFPL">Не стандартный FPL</label>
-                    </div>
 
                     <div class="form-group">
                         <label for="commentaries" id="commentaries" class="h6" > <b>Комментарий</b> </label>
                         <textarea class="form-control" id="commentaries" name="commentaries" rows="1" ></textarea>
                     </div>
+                    <div class="form-group">
+                        <label for="exempleCategory">Дирекция</label>
+                        <select class="form-control form-control-sm" id="direction" name="direction">
+                            <option>Тюмень</option>
+                            <option>Сургут</option>
+                            <option>Игарка</option>
+                            <option>Хабаровск</option>
+                            <option>Иркутск</option>
+                            <option>Ноябрьск</option>
+                            <option>Омск</option>
+                            <option>Усинск</option>
+                            <option>Уфа</option>
+                            <option>Тобольск</option>
 
+                        </select>
+                    </div>
+                    <div style="display: none">
+                        <div class="form-group text-hide hide">
+                            <label for="bdid" id="bdid" class="h6 text-hide hide" > <b>id</b> </label>
+                            <textarea class="form-control text-hide hide" id="bdid" name="bdid" rows="1" ><?php print_r(strip_tags($row['id']));?></textarea>
+                        </div>
+                    </div>
 
                     <hr>
 
