349ec6ffeff129da6922a215c2a30ff8b95a8afe
drack <drack702@gmail.com>
2019-11-23 18:50:19 +0500

    реализован обработчик редактирования FPL в БД

--- php/fpl/editFpl/editFplProcessor.php
+++ php/fpl/editFpl/editFplProcessor.php
@@ -3,46 +3,83 @@
 <?php require $_SERVER["DOCUMENT_ROOT"] . "/php/config/config.php"; ?>
 
 <?php
-var_dump($_POST);
-echo 'kokoko';
 
-exit;
-//
-////Формируем Имя плана
-//$fplName = $_POST['FPL_name']; //имя ФПЛ
-//$author = $_SESSION['user_login']; //Имя автора
-//$commentaries = $_POST['commentaries']; //комментарий к ФПЛ
-//$direction = $_POST['direction']; // Дирекция
-//
-//
-//    //если переключатель не стандартных FPL включен, то выполняется скрипт:
-//    if (isset($_POST['notStandartFPL'])){
-//        //все поля ввода назначаем переменной $fullFPL
-//        $fullFPL = new parceRMK();
-//        $fullFPL = parceRMK::ParseRMK($_POST['FPL']);
-//        $notFPL = 1;
-//
-//        //формируем запрос в базу. 15 поле - используем как хранилище информации добавленной пользователем
-//        $sqlAddFPL = 'INSERT INTO default_fpl(fplName,field15,author,direction,commentaries,notFPL)
-//                                    VALUES (:fplName,:field15,:author,:direction,:commentaries,:notFPL)';
-//
-//        $params = [':fplName' => $fplName,
-//            ':field15' => $fullFPL,
-//            ':author' => $author,
-//            ':direction' => $direction,
-//            ':commentaries' => $commentaries,
-//            ':notFPL' => $notFPL,
-//        ];
-//
-//
-//        $stmt = $pdo->prepare($sqlAddFPL);
-//        $stmt->execute($params);
-//
-//        echo 'Данные добавлены в БД.';
-//        exit;
-//
-//    }
-//
+
+//Формируем Имя плана
+$fplName = $_POST['FPL_name']; //имя ФПЛ
+$author = $_SESSION['user_login']; //Имя автора
+$commentaries = $_POST['commentaries']; //комментарий к ФПЛ
+$id = $_POST['bdid'];
+
+    //если переключатель не стандартных FPL включен, то выполняется скрипт:
+    if (isset($_POST['notStsndartFPL1'])){
+        //все поля ввода назначаем переменной $fullFPL
+        $fullFPL = new parceRMK();
+        $fullFPL = parceRMK::ParseRMK($_POST['field15']);
+        $notFPL = 1;
+
+        //формируем запрос в базу. 15 поле - используем как хранилище информации добавленной пользователем
+        $sqlAddFPL = 'UPDATE default_fpl SET fplName=:fplName, 
+                                              field15=:field15, 
+                                              author=:author,
+                                              commentaries=:commentaries 
+                                        WHERE id=:id';
+
+
+        $data = [':fplName' => $fplName,
+                ':field15' => $fullFPL,
+                ':author' => $author,
+                ':commentaries' => $commentaries,
+                ':id' => $id,
+        ];
+
+
+        $stmt = $pdo->prepare($sqlAddFPL);
+        $stmt->execute($data);
+
+        echo 'Данные обновленны';
+        exit;
+
+    }else{
+        extract($_POST);
+        //$field15 = parceRMK::ParseRMK($field15);
+        //$field18 = parceRMK::ParseRMK($field18);
+
+        $sqlEditFPL = 'UPDATE default_fpl SET fplName=:fplName,
+                                              field1=:field1,
+                                              field7=:field7,
+                                              field13=:field13,
+                                              field15=:field15,
+                                              field16=:field16,
+                                              alternative=:alternative,
+                                              direction=:direction,
+                                              commentaries=:commentaries,
+                                              field18=:field18 
+                                          WHERE id=:id';
+
+    $params = [ ':fplName' => $fplName,
+        ':field1' => $field1,
+        ':field7' => $field7,
+        ':field13' => $field13,
+        ':field15' => $field15,
+        ':field16' => $field16,
+        ':alternative' => $alternative,
+        ':direction' => $direction,
+        ':commentaries' => $commentaries,
+        ':field18' => $field18,
+        ':id' => $id];
+
+
+
+
+
+    $stmt = $pdo->prepare($sqlEditFPL);
+    $stmt->execute($params);
+
+    echo 'Данные обновлены';
+
+    }
+
 ////раскладываем FPL на составные - разделителем является "-"
 //    $fplArray = explode('-',$_POST['FPL']);
 ////проверяем количество эллементов массива - если массив, сформировался с меньшим количеством элементов
