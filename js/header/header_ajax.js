4b2df51ecfa0ac4f4134b18ccda06d2336653014
drack <drack702@gmail.com>
2019-11-27 18:49:26 +0500

    создан тестовый файл для

--- js/header/header_ajax.js
+++ js/header/header_ajax.js
@@ -1,14 +1,14 @@
 //отрисовывает страницу поиска в DOM 'content_block' файла index.php
-
-$('#findFplPage').on('click', function () {
-
-    $.ajax({
-        url: "php/fpl/findFpl/findFplPage.php",
-        cache: false,
-        success: function (data) {
-            $("#content_block").html(data);
-
-        }
-    });
-});
+//
+// $('#findFplPage').on('click', function () {
+//
+//     $.ajax({
+//         url: "php/fpl/findFpl/findFplPage.php",
+//         cache: false,
+//         success: function (data) {
+//             $("#content_block").html(data);
+//
+//         }
+//     });
+// });
 
