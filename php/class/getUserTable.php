9d6c0f8c4f5574aed85f294de651ed8f61de167c
drack <drack702@gmail.com>
2019-12-03 16:50:40 +0500

    Создан класс для отображения таблицы пользователей

---
+++ php/class/getUserTable.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Данный класс производит поиск и отображение всех пользователей
+ */
+
+class getUserTable
+
+{
+
+
+
+    public static function getDataUserInDb(){
+
+        require $_SERVER["DOCUMENT_ROOT"] . '/php/config/config.php';
+        $reqSql = "SELECT * FROM users";
+        $stmt = $pdo->prepare($reqSql);
+        $stmt->execute();
+
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+                ?>
+                <tr>
+                <th scope="row"><?php print_r($row['id']);?></th>
+                <td class="text-left"><?php print_r($row['login']);?></td>
+                <td><?php print_r($row['firstName']);?></td>
+                <td><?php print_r($row['lastName']);?></td>
+                <td><?php print_r($row['telephone']);?></td>
+                <td><input type="checkbox" id="checked-<?php print_r($row['id']);  ?>"></td>
+                </tr>
+
+                <?php
+
+            }
+        }
+
+
+public static function lookData(){
+    $obj = new getUserTable();
+    $obj ->getDataUserInDb();
+
+
+
+
+}
+}
