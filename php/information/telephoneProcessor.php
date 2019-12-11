a8b1fc89e32cb17c4dc962e7469bf2905eeedc25
drack <drack702@gmail.com>
2019-12-11 22:31:10 +0500

    Добавлена кнопка "на главную страницу"

---
+++ php/information/telephoneProcessor.php
@@ -0,0 +1,45 @@
+<?php
+
+$searchGeopointSql = "SELECT * FROM telephones";
+//$stmt->bindValue(':name', '%' . $geopointName . '%');
+$stmt = $pdo->prepare($searchGeopointSql);
+$stmt->execute();
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+
+    ?>
+
+    <div class="label-default">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-11">
+                <b><h3><?print_r($row['comment']); ?></h3></b>
+                <br>
+                <span><?print_r($row['text']); ?></span>
+            </div>
+        </div>
+
+    </div>
+
+
+    <?php
+
+}
+
+
+/**
+ *  Создал таблицу на сервере:
+ * Имя:telephones
+ *  столбцы:
+ *      id
+ *      name
+ *      number
+ *      comment
+
+ */
+?>
+
+
+
+
