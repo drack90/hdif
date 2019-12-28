62dea831a708ff3320fd2b8c4fb835061f4eade0
drack <drack702@gmail.com>
2019-12-28 06:58:26 +0500

    class for add fpl in DB

--- php/fpl/addFpl/addFplController.php
+++ php/fpl/addFpl/addFplController.php
@@ -3,5 +3,27 @@
 
 <?php
 
+//вырезаем имя изображения и переносим его в папку
+//забираем информацию о файл PATHINFO_EXTENSION (4) забирает только информацию о формате файла
+
+//
+//    $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+//    //генерируется уникальное имя + формат файла
+//    $imageName =uniqid().".".$extension;
+//    //переносим файл из папки temp в папку uploads
+//    move_uploaded_file($_FILES['image']['tmp_name'],
+//            $_SERVER['DOCUMENT_ROOT'] . '/img/fpl_image/' . $imageName);
+//    var_dump($_FILES);
+//
+//exit;
+
+
+$imageName = new addImage();
+$imageName = $imageName->uploadImage($_FILES['image']);
+var_dump($_SESSION['imageName']);
+echo $imageName;
+exit;
+
+
 $addfpl = new postFpl();
 $addfpl->addFpl();
