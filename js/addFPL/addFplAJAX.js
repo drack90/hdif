151337e97a22e7b21246f9f26e9415979d2e2500
drack <drack702@gmail.com>
2019-12-28 06:57:49 +0500

    class for add fpl in DB

--- js/addFPL/addFplAJAX.js
+++ js/addFPL/addFplAJAX.js
@@ -1,13 +1,49 @@
+var files;
+let fpl;
+// Вешаем функцию на событие
+// Получим данные файлов и добавим их в переменную
+
+
+$('input[type=file]').change(function () {
+    files = this.files;
+});
+
 //производит отправку данных на файл addFplProcessor и закрытие модального окна
- $('#addFplInBD').on('click', function () {
-     $.ajax({
-         url: "/php/fpl/addFpl/addFplProcessor.php",
-         type: "POST",
-         data: $('#addFplForm').serialize(),
-         success: function (data) {
-             alert(data);
-             console.log('kokoko');
-             $('#addFPLModal').modal('hide'); //закрывает модальное окно
-         }
-     });
- });
+$('#addFplInBD').on('click', function () {
+
+
+
+
+// Создадим данные формы и добавим в них данные файлов из files
+//РАБОТАЕТ -> файл передается, но как передать ещё данные формы?!
+    if (files != '') {
+
+        data = new FormData();
+
+        $.each(files, function (key, value) {
+            data.append('image', value);
+        });
+
+
+        $.ajax({
+            url: "/php/fpl/addFpl/addFplController.php",
+            type: "POST",
+            processData: false,
+            contentType: false, //когда делаю так - перестаёт работать
+            data: data,
+            success: function (data) {
+                //$('#addFPLModal').modal('hide'); //закрывает модальное окно
+            }
+
+        });
+    }
+    $.ajax({
+        url: "/php/fpl/addFpl/addFplController.php",
+        type: "POST",
+        data: $('#addFplForm').serialize(),
+        success: function (data) {
+            alert(data);
+            $('#addFPLModal').modal('hide'); //закрывает модальное окно
+        }
+    });
+});
