ffd11bf5f3bca7acfa1d5174c69c7ed7da27d4d2
drack <drack702@gmail.com>
2020-01-22 12:21:51 +0500

    add functional reglament

--- js/reglament/reglament.js
+++ js/reglament/reglament.js
@@ -1,44 +1,52 @@
 $(document).ready(function() {
+    let setValue= '';
     //обработчик события нажатия на кнопку.
     $('#editReglament').on('click', function () {
             $('#reglamentBody').summernote(  {height: 400}); //инициализация плагина summernote
             // $('#editReglament').html('Сохранить'); //меняем имя
              $('#editReglament').prop('value', 'true'); //меняем id
-        var setValue = {
-            'buttonValue': this.value
-        };
+             $('#sendEditableReglament').prop('style', 'display: run-in'); //меняем параметр кнопки на "видимый"
 
-        console.log(setValue);
+        /** назначаем переменной значение кнопки (true)
+        требуется для того что бы работала следующая кнопка. В противном
+        случае кнопка не отработает нажатие и будет выведенно сообщение о
+        необходимости произвести редактирование регламентов*/
+
+            setValue = $('button#editReglament').val();
+
     });
 
     //обработчик повторного нажатия на уже измененную кнопку
-    $('#sendEditableReglament').on('click',function () {
-        if ($('#editReglament'.value == 'true')) {
-            var reglamentCode = $('#reglamentBody').summernote('code'); //копируем html код summernote блока
-            reglamentCode = ({"reglamentCode": reglamentCode});
-            $('#reglamentBody').summernote('destroy');
 
 
-            $.ajax({
-                url: "/php/reglaments/editReglamentsController.php",
-                type: "POST",
-                data: reglamentCode,
-                success: function (data) {
 
-                    alert(data);
-                }
-            });
-            // $('#sendEditableReglament').html('Редактировать'); //возвращаем имя
-             $('#editReglament').prop('value', false); //возвращаем id
-            reglamentCode = null;
-            setValue = {
-                'buttonValue': $('#editReglament').value
-            };
-            console.log(setValue);
-        }
+        $('#sendEditableReglament').on('click', function () {
+          //Производим провкерку значения кнопки, и если кнопка переведена в 'true' обрабатываем событие
+            if (setValue == "true") {
+
+                var reglamentCode = $('#reglamentBody').summernote('code'); //копируем html код summernote блока
+                reglamentCode = ({"reglamentCode": reglamentCode}); //формируем в массив для корректной обработки скриптом
+                $('#reglamentBody').summernote('destroy'); //убираем инициализацию summernote
+
+                //выполняем AJAX запрос.
+                $.ajax({
+                    url: "/php/reglaments/editReglamentsController.php",
+                    type: "POST",
+                    data: reglamentCode,
+                    success: function (data) {
+                        alert(data);
+                    }
+                });
+
+                $('#editReglament').prop('value', false); //меняем value кнопки обратно
+                $('#sendEditableReglament').prop('style', 'display: none'); //прячем кнопку от глаз
+                reglamentCode = null; //обнуляем значение переменной (в случае сбоя, что бы всё равно нельзя было отправить данные)
+                setValue =  $('#editReglament').val(); //назначаем переменной обновленое значение кнопки.
+
+
+        }else{
+                alert('Сперва требуется редактировать регламенты');
+            }
     });
-
 });
 
-//сделать переключатель через переменную. Т.е. при нажатии на кнопку переменная получает значение тру, после нажатия на вторую
-//кнопку переменая получает значение false и это значение так же присваивается кнопке
